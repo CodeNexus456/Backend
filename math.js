7bb606df2b81da81c3152cf3c8bee5b48dccde01
Suraj kumar <ksurajkumar336@gmail.com>
2026-07-28 11:26:37 +0530

    add new file

--- math.js
+++ math.js
@@ -1,17 +1,7 @@
-const sum = (a, b)=> a+b;
-const sub = (a, b)=> a-b;
-const mul = (a, b)=> a*b;
+export const sum = (a, b)=> a+b;
+export const sub = (a, b)=> a-b;
+export const mul = (a, b)=> a*b;
 
-const PI = 3.14;
-const g = 9.8;
+export const PI = 3.14;
+export const g = 9.8;
 
-const obj = {
-  sum: sum,
-  sub: sub,
-  mul: mul,
-  PI: PI,
-  g: g,
-}
-
-
-module.exports = obj;
--- script.js
+++ script.js
@@ -3,8 +3,9 @@
 // console.log(math.sub(2,4));
 // console.log(math.mul(2,4));
 
-let info = require("./Fruits");
-console.log(info);
-console.log(info[0]);
-console.log(info[1]);
-console.log(info[2]);
+// let info = require("./Fruits");
+
+import {sum,sub,mul} from "./math.js";
+console.log(sum(2,4));
+console.log(sub(3,5));
+console.log(mul(3,7));
